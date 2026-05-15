f874a85ad91e646a2a3ed31c2becbbac92eb8cb2
Arthog <53008615+Arthog@users.noreply.github.com>
2026-05-15 15:53:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ st.markdown("---")
 with st.container(gap="small"):
     st.markdown(
     "**How to use this PoC Engine:** \n"
-    "* Enter a natural language request detailing the action you want to take.** \n"
+    "* Enter a natural language request detailing the action you want to take. \n"
     "* Include relevant contextual attributes such as your **role**, **location**, **network zone**, or **time of day**.\n"
     "* The agent will dynamically evaluate these attributes against corporate policy thresholds."
             )
